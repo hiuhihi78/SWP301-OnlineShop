dd98dc4d91ccc493c140960891348b5cb9828194
nepo2111 <nguyenhieuskynett@gmail.com>
2022-05-24 23:39:13 +0700

    edit admin-template

--- SWP301-OnlineShop/src/java/controller/user/LoginController.java
+++ SWP301-OnlineShop/src/java/controller/user/LoginController.java
@@ -30,7 +30,7 @@ public class LoginController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        request.getRequestDispatcher("view/admin-template/body.jsp").forward(request, response);     
+        request.getRequestDispatcher("view/admin-template/home.jsp").forward(request, response);     
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
