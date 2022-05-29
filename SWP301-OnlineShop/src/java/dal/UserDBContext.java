c9cdb61ca338a8a8aa48f6efde568cd663581010
tienpmhe151480 <tienpmhe151480@fpt.edu.vn>
2022-05-30 00:11:34 +0700

    upload new source code

--- SWP301-OnlineShop/src/java/dal/UserDBContext.java
+++ SWP301-OnlineShop/src/java/dal/UserDBContext.java
@@ -26,6 +26,35 @@ import model.User;
  */
 public class UserDBContext extends DBContext {
 
+    public User findUserById(int id) {
+        try {
+            String sql = "Select * From [User]\n"
+                    + "Where id = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, id);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt(1));
+                user.setPassword(rs.getString(2));
+                user.setAvatar(rs.getString(3));
+                user.setFullname(rs.getString(4));
+                user.setGender(rs.getBoolean(5));
+                user.setMobile(rs.getString(6));
+                user.setAddress(rs.getString(7));
+                Role role = new Role();
+                role.setId(rs.getInt(8));
+                user.setRole(role);
+                user.setStatus(rs.getBoolean(9));
+                user.setUsername(rs.getString(10));
+                user.setEmail(rs.getString(11));
+                return user;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
     public ArrayList<User> getAllUser() {
         ArrayList<User> users = new ArrayList<>();
         String sql = "SELECT [fullname]\n"
