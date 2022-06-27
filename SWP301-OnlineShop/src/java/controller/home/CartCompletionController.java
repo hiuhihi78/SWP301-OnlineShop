be6c96d98a37700471fd0190ac20ab2cd04199f1
tienpmhe151480 <tienpmhe151480@fpt.edu.vn>
2022-06-27 21:19:32 +0700

    modifier message send mail for cart

--- SWP301-OnlineShop/src/java/controller/home/CartCompletionController.java
+++ SWP301-OnlineShop/src/java/controller/home/CartCompletionController.java
@@ -153,7 +153,7 @@ public class CartCompletionController extends BaseAuthController {
             productDB.updateQuantityProductAvailable(productsOrder);
             
             // delete product ordered in cart
-            cartDB.deleteProductOrdered(productsOrder, idCart);
+//            cartDB.deleteProductOrdered(productsOrder, idCart);
             
             
             System.out.println("success");
@@ -171,6 +171,7 @@ public class CartCompletionController extends BaseAuthController {
 
             request.getRequestDispatcher("view/public/cartCompletion.jsp").forward(request, response);
         } else {
+            System.out.println("error");
             request.getRequestDispatcher("404.html").forward(request, response);
         }
 
