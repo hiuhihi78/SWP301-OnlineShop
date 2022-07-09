ef49613738108aa2d6246aba1d66ac73759f4556
hieunvhe153769 <hieunvhe153769@fpt.edu.vn>
2022-07-09 23:40:58 +0700

    edit dashboard marketing and add confirm

--- SWP301-OnlineShop/src/java/dal/DBContext.java
+++ SWP301-OnlineShop/src/java/dal/DBContext.java
@@ -19,19 +19,19 @@ public class DBContext {
 
     Connection connection;
 
-    //  public DBContext() {
-    //      try {
-    //          String username = "sa";
-    //          String password = "sa";
-    //          String url = "jdbc:sqlserver://localhost:1433;databaseName=OnlineShop2";
-    //          Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-    //          connection = DriverManager.getConnection(url, username, password);
-    //      } catch (ClassNotFoundException ex) {
-    //          Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
-    //      } catch (SQLException ex) {
-    //          Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
-    //      }
-    //  }
+      public DBContext() {
+          try {
+              String username = "sa";
+              String password = "sa";
+              String url = "jdbc:sqlserver://localhost:1433;databaseName=OnlineShop2";
+              Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+              connection = DriverManager.getConnection(url, username, password);
+          } catch (ClassNotFoundException ex) {
+              Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+          } catch (SQLException ex) {
+              Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+          }
+      }
 
 //    public DBContext() {
 //        try {
@@ -73,19 +73,19 @@ public class DBContext {
 //            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
 //        }
 //    }
-    public DBContext() {
-        try {
-            String user = "se1610";
-            String pass = "123456";
-            String url = "jdbc:sqlserver://QUANG:1433;databaseName=OnlineShop2";
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            connection = DriverManager.getConnection(url, user, pass);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+//    public DBContext() {
+//        try {
+//            String user = "se1610";
+//            String pass = "123456";
+//            String url = "jdbc:sqlserver://QUANG:1433;databaseName=OnlineShop2";
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            connection = DriverManager.getConnection(url, user, pass);
+//        } catch (ClassNotFoundException ex) {
+//            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (SQLException ex) {
+//            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
     public static void main(String[] args) {
         DBContext bContext = new DBContext();
         if (bContext.connection != null) {
